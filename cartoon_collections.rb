a67e6e9943a05dfbda4274d7c3814b97bcdbc0d1
David masterson <david73180@gmail.com>
2018-09-23 15:54:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,8 @@ def find_the_cheese(array)
 cheese_types = "cheddar", "gouda", "camembert"
   cheese_types.find do |cheese|
     array.include? (cheese_types)
+    return cheese
+    break
   end
 end
   
