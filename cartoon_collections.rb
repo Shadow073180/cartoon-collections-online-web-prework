0ec3eb1f2988ba26d4406b7f1e5c132ee26c5a8c
David masterson <david73180@gmail.com>
2018-09-23 16:11:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_type = "cheddar", "gouda", "camembert"
+  cheese_type = ["cheddar", "gouda", "camembert"]
   array.include?(cheese_type)
 end
   
