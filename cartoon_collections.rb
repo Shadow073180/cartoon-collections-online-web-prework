808cb6f40eff1021ea9681d6b7c4dafc3772d9ff
David masterson <david73180@gmail.com>
2018-09-23 16:09:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,18 +26,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  puts array
-  new_array = array.each do |x|
-    new_array.push(x)
-  end
-cheese_types = "cheddar", "gouda", "camembert"
-  new_array.include? (cheese_types)
-   new_array.find do |cheese|
-      if new_array.include?(cheese)
-        return cheese
-      else
-        return nil
-      end
-   end 
+  return array
 end
   
